4f1abb1165ac72c65c81d733da06cfc26a8e60d7
Daniel Schauenberg <d@unwiredcouch.com>
2015-12-27 19:23:29 -0500

    add REDIS_URL as fallback URI
    this makes it possible to use the Heroku redis service while keeping the
    default as redistogo

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'redis'
 module Katana
     class App < Guillotine::App
       # use redis adapter with redistogo
-      uri = URI.parse(ENV["REDISTOGO_URL"])
+      uri = URI.parse(ENV["REDISTOGO_URL"] || ENV["REDIS_URL"])
       REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
       adapter = Guillotine::Adapters::RedisAdapter.new REDIS
       set :service => Guillotine::Service.new(adapter, :strip_query => false,
